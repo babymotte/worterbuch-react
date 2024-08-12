254032d4b1704e40d8f01285718d43cd35fda1de
Michael Bachmann <mbachmann@bbmsoft.net>
2024-08-12 19:35:21 +0200

    implemented quiet pDelete + wbState + promises instead of callbacks

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,38 @@
-import { useSubscribe } from "worterbuch-react";
+import React from "react";
+import { useCachedSubscribe, useSubscribe, useWbState } from "worterbuch-react";
 
 function App() {
   const uptime = useSubscribe<number>("$SYS/uptime");
-  return <div className="App">$SYS/uptime: {uptime}</div>;
+
+  const [counter, setCounter] = useWbState<number>(
+    "worterbuch-react/demo/state",
+    0
+  );
+
+  const hello = useCachedSubscribe("hello");
+
+  return (
+    <div className="App">
+      <div>$SYS/uptime: {uptime}</div>
+      <div>
+        <div>Counter: {`${counter}`}</div>
+        <div>
+          <button
+            onClick={() =>
+              (
+                setCounter as React.Dispatch<
+                  React.SetStateAction<number | undefined>
+                >
+              )((v: number | undefined) => (v ? v + 1 : 1))
+            }
+          >
+            +
+          </button>
+        </div>
+        <div>Hello {`${hello}`}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -23,6 +23,8 @@ import {
   KeyValuePairs,
   Children,
   Value,
+  KeyValuePair,
+  PStateEvent,
 } from "worterbuch-js";
 import React from "react";
 
@@ -188,15 +190,15 @@ export function Worterbuch({
 }
 
 export function useGetLater<T extends Value>(): (
-  key: string,
-  consumer: (value: T | undefined) => void
-) => void {
+  key: string
+) => Promise<T | undefined> {
   const wb = React.useContext(WbContext);
   return React.useCallback(
-    (key: string, consumer: (value: T | undefined) => void) => {
+    (key: string) => {
       if (wb.connection) {
-        wb.connection.get<T>(key).then(consumer);
+        return wb.connection.get<T>(key);
       }
+      return Promise.resolve(undefined);
     },
     [wb.connection]
   );
@@ -204,16 +206,14 @@ export function useGetLater<T extends Value>(): (
 
 export function useGet<T extends Value>(
   key: string
-): (consumer: (value: T | undefined) => void) => void {
+): () => Promise<T | undefined> {
   const wb = React.useContext(WbContext);
-  return React.useCallback(
-    (consumer: (value: T | undefined) => void) => {
-      if (wb.connection) {
-        wb.connection.get<T>(key).then((val) => consumer(val as T));
-      }
-    },
-    [wb.connection, key]
-  );
+  return React.useCallback(() => {
+    if (wb.connection) {
+      return wb.connection.get<T>(key);
+    }
+    return Promise.resolve(undefined);
+  }, [wb.connection, key]);
 }
 
 export function useGetOnce<T extends Value>(key: string): T | undefined {
@@ -231,15 +231,15 @@ export function useGetOnce<T extends Value>(key: string): T | undefined {
 }
 
 export function useDeleteLater<T extends Value>(): (
-  key: string,
-  consumer?: (value: T | undefined) => void
-) => void {
+  key: string
+) => Promise<Value | undefined> {
   const wb = React.useContext(WbContext);
   return React.useCallback(
-    (key: string, consumer?: (value: T | undefined) => void) => {
+    (key: string) => {
       if (wb.connection) {
-        wb.connection.delete<T>(key).then(consumer);
+        return wb.connection.delete<T>(key);
       }
+      return Promise.resolve(undefined);
     },
     [wb.connection]
   );
@@ -247,39 +247,42 @@ export function useDeleteLater<T extends Value>(): (
 
 export function useDelete<T extends Value>(
   key: string
-): (consumer?: (value: T | undefined) => void) => void {
+): () => Promise<Value | undefined> {
   const wb = React.useContext(WbContext);
-  return React.useCallback(
-    (consumer?: (value: T | undefined) => void) => {
-      if (wb.connection) {
-        wb.connection.delete<T>(key).then((val) => {
-          if (consumer) consumer(val);
-        });
-      }
-    },
-    [wb.connection, key]
-  );
+  return React.useCallback(() => {
+    if (wb.connection) {
+      return wb.connection.delete<T>(key);
+    }
+    return Promise.resolve(undefined);
+  }, [wb.connection, key]);
 }
 
-export function usePDeleteLater<T extends Value>(): (key: string) => void {
+export function usePDeleteLater<T extends Value>(): (
+  pattern: string,
+  quiet?: boolean
+) => Promise<KeyValuePair<T>[] | undefined> {
   const wb = React.useContext(WbContext);
   return React.useCallback(
-    (key: string) => {
+    (pattern: string, quiet?: boolean) => {
       if (wb.connection) {
-        wb.connection.pDelete<T>(key);
+        return wb.connection.pDelete<T>(pattern, quiet);
       }
+      return Promise.resolve(undefined);
     },
     [wb.connection]
   );
 }
 
-export function usePDelete<T extends Value>(key: string): () => void {
+export function usePDelete<T extends Value>(
+  pattern: string,
+  quiet?: boolean
+): () => void {
   const wb = React.useContext(WbContext);
   return React.useCallback(() => {
     if (wb.connection) {
-      wb.connection.pDelete<T>(key);
+      wb.connection.pDelete<T>(pattern, quiet);
     }
-  }, [wb.connection, key]);
+  }, [wb.connection, pattern, quiet]);
 }
 
 export function useSubscribe<T extends Value>(
@@ -291,8 +294,9 @@ export function useSubscribe<T extends Value>(
   const wb = React.useContext(WbContext);
   const [value, setValue] = React.useState<T | undefined>(initialValue);
   React.useEffect(() => {
-    if (wb.connection) {
-      const sub = wb.connection.subscribe<T>(
+    const conn = wb.connection;
+    if (conn) {
+      const sub = conn.subscribe<T>(
         key,
         ({ value }) => {
           setValue(value);
@@ -301,8 +305,8 @@ export function useSubscribe<T extends Value>(
         liveOnly
       );
       return () => {
-        if (wb.connection) {
-          wb.connection.unsubscribe(sub);
+        if (conn) {
+          conn.unsubscribe(sub);
         }
       };
     } else {
@@ -319,10 +323,7 @@ export function usePSubscribe<T extends Value>(
 ) {
   const wb = React.useContext(WbContext);
   const [values, update] = React.useReducer(
-    (
-      state: Map<Key, T>,
-      event: { keyValuePairs?: KeyValuePairs<T>; deleted?: KeyValuePairs<T> }
-    ) => {
+    (state: Map<Key, T>, event: PStateEvent<T>) => {
       if (event.keyValuePairs) {
         event.keyValuePairs.forEach((kvp) => {
           state.set(kvp.key, kvp.value);
@@ -338,11 +339,12 @@ export function usePSubscribe<T extends Value>(
     new Map()
   );
   React.useEffect(() => {
-    if (wb.connection) {
-      const sub = wb.connection.pSubscribe<T>(key, update, unique, liveOnly);
+    const conn = wb.connection;
+    if (conn) {
+      const sub = conn.pSubscribe<T>(key, update, unique, liveOnly);
       return () => {
-        if (wb.connection) {
-          wb.connection.unsubscribe(sub);
+        if (conn) {
+          conn.unsubscribe(sub);
         }
       };
     }
@@ -406,33 +408,30 @@ export function usePublish<T extends Value>(key: string) {
 }
 
 export function useLsLater(): (
-  parent: string | undefined,
-  consumer: (children: Children) => void
-) => void {
+  parent: string | undefined
+) => Promise<Children | undefined> {
   const wb = React.useContext(WbContext);
   return React.useCallback(
-    (parent: string | undefined, consumer: (children: Children) => void) => {
+    (parent: string | undefined) => {
       if (wb.connection) {
-        wb.connection.ls(parent).then(consumer);
+        return wb.connection.ls(parent);
       }
+      return Promise.resolve(undefined);
     },
     [wb.connection]
   );
 }
 
 export function usePLsLater(): (
-  parentPattern: string | undefined,
-  consumer: (children: Children) => void
-) => void {
+  parentPattern: string | undefined
+) => Promise<Children | undefined> {
   const wb = React.useContext(WbContext);
   return React.useCallback(
-    (
-      parentPattern: string | undefined,
-      consumer: (children: Children) => void
-    ) => {
+    (parentPattern: string | undefined) => {
       if (wb.connection) {
-        wb.connection.pLs(parentPattern).then(consumer);
+        return wb.connection.pLs(parentPattern);
       }
+      return Promise.resolve(undefined);
     },
     [wb.connection]
   );
@@ -440,30 +439,26 @@ export function usePLsLater(): (
 
 export function useLs(
   parent: string | undefined
-): (consumer: (children: Children) => void) => void {
+): () => Promise<Children | undefined> {
   const wb = React.useContext(WbContext);
-  return React.useCallback(
-    (consumer: (children: Children) => void) => {
-      if (wb.connection) {
-        wb.connection.ls(parent).then(consumer);
-      }
-    },
-    [wb.connection, parent]
-  );
+  return React.useCallback(() => {
+    if (wb.connection) {
+      return wb.connection.ls(parent);
+    }
+    return Promise.resolve(undefined);
+  }, [wb.connection, parent]);
 }
 
 export function usePLs(
   parentPattern: string | undefined
-): (consumer: (children: Children) => void) => void {
+): () => Promise<Children | undefined> {
   const wb = React.useContext(WbContext);
-  return React.useCallback(
-    (consumer: (children: Children) => void) => {
-      if (wb.connection) {
-        wb.connection.pLs(parentPattern).then(consumer);
-      }
-    },
-    [wb.connection, parentPattern]
-  );
+  return React.useCallback(() => {
+    if (wb.connection) {
+      return wb.connection.pLs(parentPattern);
+    }
+    return Promise.resolve(undefined);
+  }, [wb.connection, parentPattern]);
 }
 
 export function useLsOnce(parent: string | undefined): string[] {
@@ -492,11 +487,12 @@ export function useSubscribeLs(parent: string | undefined): Children {
   const wb = React.useContext(WbContext);
   const [children, setChildren] = React.useState<Children>([]);
   React.useEffect(() => {
-    if (wb.connection) {
-      const sub = wb.connection.subscribeLs(parent, setChildren);
+    const conn = wb.connection;
+    if (conn) {
+      const sub = conn.subscribeLs(parent, setChildren);
       return () => {
-        if (wb.connection) {
-          wb.connection.unsubscribeLs(sub);
+        if (conn) {
+          conn.unsubscribeLs(sub);
         }
       };
     }
@@ -563,27 +559,43 @@ export function useSetGraveGoodsLater() {
 
 export function useWbState<T extends Value>(key: string, initialValue?: T) {
   const wb = React.useContext(WbContext);
+
   const [state, setState] = React.useState<T | undefined>(initialValue);
-  const stateRef = React.useRef<T | undefined>(state);
+  const [connected, setConnected] = React.useState(false);
+
   React.useEffect(() => {
-    if (state === undefined) {
-      wb.connection?.delete(key);
-    } else {
-      wb.connection?.set(key, state);
-    }
-    stateRef.current = state;
-  }, [key, state, wb.connection]);
-  React.useEffect(() => {
-    wb.connection?.subscribe<T>(key, ({ value }) => {
-      if (!deepEqual(value, stateRef.current)) {
-        if (value !== null) {
-          setState(value);
-        } else {
-          setState(undefined);
+    const conn = wb.connection;
+    if (!connected) {
+      conn?.get<T>(key).then((v) => {
+        if (!connected && v === undefined) {
+          conn?.set(key, initialValue || null);
         }
-      }
+      });
+    }
+  }, [connected, initialValue, key, wb.connection]);
+
+  React.useEffect(() => {
+    const conn = wb.connection;
+    const tid = conn?.cached()?.subscribe<T>(key, (value: T | undefined) => {
+      setState(value);
+      setConnected(true);
     });
+    return () => {
+      if (tid !== undefined) {
+        conn?.unsubscribe(tid);
+      }
+    };
   }, [key, wb.connection]);
+
+  React.useEffect(() => {
+    if (connected) {
+      if (state === undefined) {
+        wb.connection?.cached()?.delete(key);
+      } else {
+        wb.connection?.cached()?.set(key, state);
+      }
+    }
+  }, [connected, key, state, wb.connection]);
 
   return [state, setState];
 }
@@ -599,51 +611,78 @@ export function useExpireCache(maxAge: number, interval?: number) {
 
 export function useCachedGet<T extends Value>(
   key: string
-): (consumer: (value: T | undefined) => void) => void {
+): () => Promise<T | undefined> {
+  const wb = React.useContext(WbContext);
+  return React.useCallback(() => {
+    if (wb.connection) {
+      return wb.connection.cached().get(key);
+    }
+    return Promise.resolve(undefined);
+  }, [wb.connection, key]);
+}
+
+export function useCachedGetLater<T extends Value>(): (
+  key: string
+) => Promise<T | undefined> {
   const wb = React.useContext(WbContext);
   return React.useCallback(
-    (consumer: (value: T | undefined) => void) => {
+    (key: string) => {
       if (wb.connection) {
-        wb.connection
-          .cached()
-          .get(key)
-          .then((val) => consumer(val as T));
+        return wb.connection.cached().get(key);
+      }
+      return Promise.resolve(undefined);
+    },
+    [wb.connection]
+  );
+}
+
+export function useCachedSet<T extends Value>(key: string): (value: T) => void {
+  const wb = React.useContext(WbContext);
+  return React.useCallback(
+    (value: T) => {
+      if (wb.connection) {
+        wb.connection.cached().set<T>(key, value);
       }
     },
     [wb.connection, key]
   );
 }
 
-function deepEqual(obj1: Value | undefined, obj2: Value | undefined) {
-  // Base case: If both objects are identical, return true.
-  if (obj1 === obj2) {
-    return true;
-  }
-  // Check if both objects are objects and not null.
-  if (
-    typeof obj1 !== "object" ||
-    typeof obj2 !== "object" ||
-    obj1 == null ||
-    obj2 == null
-  ) {
-    return false;
-  }
-  // Get the keys of both objects.
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  // Check if the number of keys is the same.
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-  // Iterate through the keys and compare their values recursively.
-  for (const key of keys1) {
-    if (
-      !keys2.includes(key) ||
-      !deepEqual((obj1 as any)[key], (obj2 as any)[key])
-    ) {
-      return false;
+export function useCachedSetLater<T extends Value>(): (
+  key: string,
+  value: T
+) => void {
+  const wb = React.useContext(WbContext);
+  return React.useCallback(
+    (key: string, value: T) => {
+      if (wb.connection) {
+        wb.connection.cached().set<T>(key, value);
+      }
+    },
+    [wb.connection]
+  );
+}
+
+export function useCachedSubscribe<T extends Value>(
+  key: string,
+  initialValue?: T
+): T | undefined {
+  const wb = React.useContext(WbContext);
+  const [value, setValue] = React.useState<T | undefined>(initialValue);
+  React.useEffect(() => {
+    const conn = wb.connection;
+    if (conn) {
+      const sub = conn.cached().subscribe<T>(key, (v: T | undefined) => {
+        setValue(v);
+      });
+      return () => {
+        if (conn) {
+          conn.unsubscribe(sub);
+        }
+      };
+    } else {
+      setValue(undefined);
     }
-  }
-  // If all checks pass, the objects are deep equal.
-  return true;
+  }, [key, wb.connection]);
+  return value;
 }
